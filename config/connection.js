ecc2840fda24dc15ab373707363d548ff4d2f015
Hetal Patel <nphetal1905@gmail.com>
2019-08-18 16:10:47 -0400

    added env file

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,43 @@
 var mysql = require("mysql");
+require("dotenv").config();
 
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.sqlPassword, 
+    database: "burgers_db"
+  });
+}
+
+connection.config.typeCast = function(field, next) {
+  if (field.type == "TINY" && field.length == 1) {
+    return field.string() == "1"; // 1 = true, 0 = false
   }
-  console.log("connected as id " + connection.threadId);
-});
+  return next();
+};
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "burgers_db"
+// });
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 module.exports = connection;
+// ---------------
+
